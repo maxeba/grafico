1d538ee69fe4a8beea8bc5fe30d35cee7cceb233
maxeba <105441023+maxeba@users.noreply.github.com>
2024-10-03 11:51:47 -0300

    aula 5

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,17 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+}
+
+vizualizarInformacoesGlobais()
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados await res.json()
+    const paragrafo document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+  }
+  
+  vizualizarInformacoesGlobais()
